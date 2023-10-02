b02725cd6e62b5862787696016b507114730be06
Leapril <35242597+Leapril@users.noreply.github.com>
2023-10-02 13:36:52 -0700

    Added a hello world program.
    My repository has its first file!

---
+++ hello-world.py
@@ -0,0 +1,14 @@
+#This program will write 
+#Hello World!
+
+#defines a custom function
+def PrintHelloWorld():
+	print("Hello World!")
+
+#defines main function
+def main():
+	PrintHelloWorld()
+
+#failsafe execution
+ if __name__=="__main__":
+	main()
