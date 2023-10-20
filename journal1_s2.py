983af0edc2f8f6d53d314446e5ba04117d46c4b3
Leapril <35242597+Leapril@users.noreply.github.com>
2023-10-20 15:25:55 -0700

    Update journal1_s2.py

--- journal1_s2.py
+++ journal1_s2.py
@@ -6,28 +6,30 @@
 
 #functions:add,subctract,and multiply which will take the values from main, 
 #then print the results
-def add(a,b):
+def add(a,b): #addition
 	s = a+b 
-	print("Addition result: ", s)
+	print("Addition result: ", s) #prints results
 
-def subtract(a,b):
+def subtract(a,b): #subtraction
 	s = a-b
 	print("Subtraction result: ", s)
 
-def multiply(a,b):
+def multiply(a,b): #multiplication
 	s = a*b
 	print("Multiplication result: ", s)
 
 def main():
+	#create values a,d,c
 	a = 1.37 #float
 	b = 4.63 #float
 	c = 6 	 #int
 
-	#callign functions with values
+	#calling functions with values
 	add(a,b) 
 	subtract(a,b)
 	multiply(a,c)
 
+#failsafe executable
 if __name__=="__main__":
 	main()
 
